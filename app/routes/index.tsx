410616c917dfbdfabfc5d16d8956a95c6a59241c
Logan McAnsh <logan@mcan.sh>
2023-03-17 11:09:39 -0400

    move root meta to head
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,43 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "New Remix App",
+  };
+};
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
+          <Link
             target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+            to="https://remix.run/tutorials/blog"
+            rel="noreferrer noopener"
           >
             15m Quickstart Blog Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a
+          <Link
             target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
+            to="https://remix.run/tutorials/jokes"
+            rel="noreferrer noopener"
           >
             Deep Dive Jokes App Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <Link
+            target="_blank"
+            to="https://remix.run/docs"
+            rel="noreferrer noopener"
+          >
             Remix Docs
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
